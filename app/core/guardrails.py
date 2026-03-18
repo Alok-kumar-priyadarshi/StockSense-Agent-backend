6fb8a36d41e0178a6c63d6f59154edc06c3a94b3
alok kumar priyadarshi <155714900+Alok-kumar-priyadarshi@users.noreply.github.com>
2026-03-18 22:30:17 +0530

    Update guardrails.py

--- app/core/guardrails.py
+++ app/core/guardrails.py
@@ -2,11 +2,17 @@ def apply_guardrails(response: dict):
 
     text = str(response).lower()
 
-    forbidden = ["buy", "sell", "invest", "guaranteed" , "purchase" , "recommend","bomb","guns","drugs","Trafficking"  ]
+    forbidden = ["buy", "sell", "invest", "guaranteed" , "purchase" , "recommend" , "portfolio"  ]
+    forbidden2 = ["bomb","guns","drugs","Trafficking","fuck"] 
+    
 
     if any(word in text for word in forbidden):
         return {
             "error": "This system does not provide financial advice"
+        }
+    if any(word in text for word in forbidden2):
+        return {
+            "error": "This system does not accept critical object names"
         }
 
     # ensure confidence exists
